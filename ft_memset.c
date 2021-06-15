c84e862bffed1771dfcb7fcf93c5973ea76b2c79
Jonathan Fritz <jfritz@1-B-5.42heilbronn.de>
2021-06-15 13:53:14 +0200

    Finished a few functions

---
+++ ft_memset.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jfritz <jfritz@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/15 13:27:12 by jfritz            #+#    #+#             */
+/*   Updated: 2021/06/15 13:52:51 by jfritz           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+void *ft_memset(void *b, int c, size_t len)
+{
+	unsigned int	lenc;
+	unsigned char	d;
+	unsigned char	*t;
+
+	lenc = 0;
+	lenc = 0;
+	d = (unsigned char)c;
+	t = (unsigned char*)b;
+	while (len > lenc)
+	{
+		t[lenc] = d;
+		lenc++;
+	}
+}
