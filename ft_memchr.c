03080d1c4d51ee8032eccaf9121171b3ee779748
Jonathan Fritz <jfritz@1-B-5.42heilbronn.de>
2021-06-17 16:34:48 +0200

    memchr

---
+++ ft_memchr.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jfritz <jfritz@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/17 16:25:48 by jfritz            #+#    #+#             */
+/*   Updated: 2021/06/17 16:34:23 by jfritz           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	int		i;
+	unsigned char	*g;
+	unsigned char	*d;
+	int		t;
+
+	i = 0;
+	t = 0;
+	d = (unsigned char *) s;
+	while (n > i)
+	{
+		if ((d[i] != (unsigned char *)c))
+			i++;
+		else
+		{
+			g = (unsigned char *) &s[i];
+			return (g);
+		}
+	}
+	return (0);
+}
