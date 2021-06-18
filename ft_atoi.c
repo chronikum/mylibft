d05b1318d6842383f11c8d7e9ac1f3aabe57bd54
Jonathan Fritz <jfritz@1-B-7.42heilbronn.de>
2021-06-18 15:10:16 +0200

    Fix

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,13 +6,13 @@
 /*   By: jfritz <jfritz@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/15 15:13:18 by jfritz            #+#    #+#             */
-/*   Updated: 2021/06/18 14:58:11 by jfritz           ###   ########.fr       */
+/*   Updated: 2021/06/18 15:09:55 by jfritz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_isdigit(char c);
+int	ft_isdigit(int c);
 
 int	whitespace(char c)
 {
@@ -26,17 +26,17 @@ int	ft_skip_plus_minus(const char *str, int p, int *m)
 	int	a;
 
 	a = 0;
-	if (str[p] == '-' && ft_isdigit(str[p + 1]))
+	if (str[p] == '-' && ft_isdigit((int) str[p + 1]))
 	{
 		*m = -1;
 		a++;
 	}
-	if (str[p] == '+' && ft_isdigit(str[p + 1]))
+	if (str[p] == '+' && ft_isdigit((int) str[p + 1]))
 	{
 		*m = 1;
 		a++;
 	}
-	if (ft_isdigit(str[p]))
+	if (ft_isdigit((int) str[p]))
 		*m = 1;
 	return (p + a);
 }
@@ -57,7 +57,7 @@ int	ft_atoi(const char *str)
 		return (0);
 	if (str[i] == '0')
 		i++;
-	while (ft_isdigit(str[i]) && (str[i] != '\0'))
+	while (ft_isdigit((int) str[i]) && (str[i] != '\0'))
 	{
 		r = ((10 * r) + (str[i] - 48));
 		i++;
