8177dab5bf2b9f5bd8c99162c4247d5363146a7d
Jonathan Fritz <jfritz@1-B-7.42heilbronn.de>
2021-06-18 10:50:36 +0200

    mem cpy

---
+++ ft_memcpy.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jfritz <jfritz@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/18 10:41:00 by jfritz            #+#    #+#             */
+/*   Updated: 2021/06/18 10:50:11 by jfritz           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	unsigned int	e;
+	unsigned char	*d;
+	unsigned char	*s;
+
+	d = (unsigned char *) dst;
+	s = (unsigned char *) src;
+	e = 0;
+	while (n > e)
+	{
+		d[e] = s[e];
+		e++;
+	}
+	return (dst);
+}
