0d85e278f0d6c1bab482ecf29e8777a8e8d81dff
Jonathan Fritz <jfritz@1-B-7.42heilbronn.de>
2021-06-18 16:03:17 +0200

    memccpy now working

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: jfritz <jfritz@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/15 13:54:01 by jfritz            #+#    #+#             */
-/*   Updated: 2021/06/18 14:58:39 by jfritz           ###   ########.fr       */
+/*   Updated: 2021/06/18 16:02:55 by jfritz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,15 @@ void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
 	unsigned char	*d;
 	unsigned char	*s;
 
+	if (dst == 0 && src == 0)
+		return (0);
 	d = (unsigned char *) dst;
 	s = (unsigned char *) src;
 	e = 0;
 	while (n > e)
 	{
-		d[e] = s[e];
-		if (s[e] == c)
+		d[e] = (unsigned char) s[e];
+		if (s[e] == (unsigned char) c)
 		{
 			return ((void *) &dst[e + 1]);
 		}
