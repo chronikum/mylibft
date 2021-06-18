e8d8b817c3121c1fa29682fe00faf78cfbf152c9
Jonathan Fritz <jfritz@1-B-7.42heilbronn.de>
2021-06-18 13:56:52 +0200

    memmove placeholder code

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,11 +6,15 @@
 /*   By: jfritz <jfritz@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/18 10:51:12 by jfritz            #+#    #+#             */
-/*   Updated: 2021/06/18 10:51:27 by jfritz           ###   ########.fr       */
+/*   Updated: 2021/06/18 13:56:39 by jfritz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stddef.h>
+
 void	*memmove(void *dst, const void *src, size_t len)
 {
-	
+	if (dst && src && len)
+		return 0;
+	return 0;
 }
