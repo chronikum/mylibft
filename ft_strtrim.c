2dd8809f4bc1715cee921d84ebea974aa7c224af
Jonathan Fritz <fritz@nosc.io>
2021-06-19 18:33:22 +0200

    strtrim

---
+++ ft_strtrim.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jfritz <jfritz@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/19 16:35:22 by jfritz            #+#    #+#             */
+/*   Updated: 2021/06/19 18:33:09 by jfritz           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlen(const char *s);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strdup(const char *s1);
+
+int	ft_inset(char c, char const *set)
+{
+	int	count;
+
+	count = 0;
+	while (set[count])
+	{
+		if (c == set[count])
+			return (1);
+		count++;
+	}
+	return (0);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	unsigned int	l;
+	int				start;
+	int				end;
+	char			*r;
+
+	end = ft_strlen(s1);
+	start = 0;
+	l = ft_strlen(s1);
+	if (ft_strlen(set) == 0)
+		return (ft_strdup(s1));
+	while (ft_inset(s1[start], set) == 1)
+	{
+		start++;
+		l--;
+	}
+	while (ft_inset(s1[end - 1], set) == 1)
+	{
+		end--;
+		l--;
+	}
+	r = ft_substr(s1, start, l);
+	if (!r)
+		return (0);
+	return (r);
+}
