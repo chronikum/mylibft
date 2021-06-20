e421274014d7555b1106151ee184e7b902db6f60
Jonathan Fritz <fritz@nosc.io>
2021-06-20 20:14:49 +0200

    norm fix

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -6,7 +6,7 @@
 /*   By: jfritz <jfritz@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/19 23:11:23 by jfritz            #+#    #+#             */
-/*   Updated: 2021/06/19 23:14:15 by jfritz           ###   ########.fr       */
+/*   Updated: 2021/06/20 20:11:50 by jfritz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putchar_fd(char c, int fd);
 
-void ft_putendl_fd(char *s, int fd)
+void	ft_putendl_fd(char *s, int fd)
 {
 	if (!s)
 		return ;
--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: jfritz <jfritz@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/19 18:38:10 by jfritz            #+#    #+#             */
-/*   Updated: 2021/06/20 18:02:10 by jfritz           ###   ########.fr       */
+/*   Updated: 2021/06/20 20:14:37 by jfritz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,15 +27,14 @@ size_t	ft_strlen_char(const char *s, char c)
 	size_t	d;
 
 	n = 0;
-	next = ft_strchr((const char *) &s[1],c);
+	next = ft_strchr((const char *) &s[1], c);
 	if (!next)
 		return (0);
 	d = (next - s) * sizeof(char);
-	return d;
+	return (d);
 }
 
-
-char *ft_get_index(char *s, char c, int p)
+char	*ft_get_index(char *s, char c, int p)
 {
 	unsigned int	x;
 	int				u;
@@ -50,35 +49,34 @@ char *ft_get_index(char *s, char c, int p)
 			if (p == u)
 			{
 				l = ft_strlen_char(&s[x], c);
-				// printf("%s, x %u, l %zu\n SEP: %c ", &s[x], x, l, c);
-				return ft_substr(&s[x], x, l);
+				return (ft_substr(&s[x], x, l));
 			}
 			u++;
 		}
 		x++;
 	}
-	return &s[p];
+	return (&s[p]);
 }
 
-int ft_word_count(char *s, char c)
+int	ft_word_count(char *s, char c)
 {
 	int	d;
 	int	w;
+
 	d = 0;
 	w = 1;
-
 	if (ft_strlen(s) == 0)
-		return 0;
+		return (0);
 	while (d < (int) ft_strlen(s))
 	{
 		d++;
 		if (s[d] == c)
 			w++;
 	}
-	return w;
+	return (w);
 }
 
-char **ft_split(char const *s, char c)
+char	**ft_split(char const *s, char c)
 {
 	char			**d;
 	unsigned int	w;
@@ -86,15 +84,12 @@ char **ft_split(char const *s, char c)
 
 	e = 0;
 	w = ft_word_count((char *) s, c);
-	d = (char**)malloc(sizeof(char*) * w);
-
+	d = (char **)malloc(sizeof(char *) * w);
 	while (e < w)
 	{
 		d[e] = ft_get_index((char *) s, c, e);
-		// printf("\n\n%s", d[e]);
 		e++;
 	}
-
 	if (!d)
 		return (NULL);
 	return (d);
