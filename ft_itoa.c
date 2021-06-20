47654e0387d8a50b3d259b29defac95a4ab2e25c
Jonathan Fritz <fritz@nosc.io>
2021-06-20 17:51:34 +0200

    Updated itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: jfritz <jfritz@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/20 16:58:31 by jfritz            #+#    #+#             */
-/*   Updated: 2021/06/20 17:38:31 by jfritz           ###   ########.fr       */
+/*   Updated: 2021/06/20 17:51:26 by jfritz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,24 +17,19 @@
 void	ft_convert_to_char(int n, char* str, size_t len)
 {
 	unsigned int	t;
-	char 			c;
 	static size_t	i = 0;
-
 
 	t = 0;
 	i++;
-	c = n + 48;
-	if (n > 9) 
+	if (n > 9)
 	{
 		t = n % 10;
 		n /= 10;
-		printf("%d %d\n", t, (len - i));
-		ft_convert_to_char(n, i, len);
+		str[(len - i)] = (char) t + 48;
+		ft_convert_to_char(n, str, len);
 	} 
 	else 
-	{
-			printf("%d %d\n", n, (len - i));
-	}
+		str[(len - i)] = (char) n + 48;
 }
 
 int ft_numberlength(int n)
@@ -56,23 +51,10 @@ int ft_numberlength(int n)
 
 char *ft_itoa(int n)
 {
-	int l;
 	char *str;
-	int t;
-	int o;
-	int x;
-
-	o = n;
-	x = 0;
+	
 	str = (char *)malloc((sizeof(char) * ft_numberlength(n)) + 1);
 	ft_convert_to_char(n, str, ft_numberlength(n));
-	// printf("TEST: %s", c);
-	return "a";
-}
-
-int	main()
-{
-	int d = 12345;
-	char *s = ft_itoa(d);
-	// printf("\n%s", s);
+	str[ft_numberlength(n) + 1] = '\0';
+	return str;
 }
