ed44a360ffd198fe1ae28aab527314d2a8fdd371
Jonathan Fritz <fritz@nosc.io>
2021-06-20 19:36:47 +0200

    itoa working now

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: jfritz <jfritz@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/20 16:58:31 by jfritz            #+#    #+#             */
-/*   Updated: 2021/06/20 18:44:09 by jfritz           ###   ########.fr       */
+/*   Updated: 2021/06/20 19:36:34 by jfritz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,54 +14,17 @@
 
 #include <stdio.h>
 
-char	*ft_strdup(const char *s1);
-size_t	ft_strlen(const char *s);
-
-char	*ft_char_biggest(int n)
-{
-	if (n == -2147483648)
-	{
-		return "-2147483648";
-	}
-
-	if (n == 2147483647)
-	{
-		return "2147483648";
-	}
-
-	return 0;
-}
-
-void	ft_convert_to_char(int n, char* str, size_t len)
-{
-	unsigned int	t;
-	static size_t	i = 0;
-
-	t = 0;
-	i++;
-	if (n > 9)
-	{
-		t = n % 10;
-		n /= 10;
-		str[(len - i)] = (char) t + 48;
-		ft_convert_to_char(n, str, len);
-	} 
-	else 
-		str[(len - i)] = (char) n + 48;
-	str[(len)] = '\0';
-}
-
 int ft_numberlength(int n)
 {
 	int o;
-	int t;
 	int l;
 
 	o = n;
 	l = 0;
+	if (n < 0)
+		o = -o;
 	while (o > 0)
 	{
-		t = o % 10;
 		o /= 10;
 		l++;
 	}
@@ -70,30 +33,32 @@ int ft_numberlength(int n)
 
 char *ft_itoa(int n)
 {
-	char *str;
 	int neg;
-
+	char *str;
+	int length;
+	
+	neg = 0;
 	if (n == -2147483648)
 		return (ft_strdup("-2147483648"));
 	if (n == 0)
 		return (ft_strdup("0"));
-	neg = 0;
+	length = ft_numberlength(n);
 	if (n < 0)
 	{
 		neg = 1;
 		n = -n;
 	}
 	str = (char *)malloc((sizeof(char) * ft_numberlength(n)) + 1 + neg);
+	if (!str)
+		return (0);
 	if (neg == 1)
 		str[0] = '-';
-	ft_convert_to_char(n, &str[neg], ft_numberlength(n));
-	// str[ft_numberlength(n) + 1] = '\0';
+	str[ft_numberlength(n) + neg] = '\0'; 
+	while (length)
+	{
+		length--;
+		str[length + neg] = (n % 10) + '0';
+		n /= 10;
+	}
 	return str;
-}
-
-int main()
-{
-	int n = -123123;
-	char *c = ft_itoa(n);
-	printf("%s", c);
 }
