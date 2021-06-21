e177e55605f4ab45ec2cd7926ae3b02b33a640bb
Jonathan Fritz <jfritz@1-B-5.42heilbronn.de>
2021-06-21 11:21:47 +0200

    Word count working now+

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: jfritz <jfritz@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/19 18:38:10 by jfritz            #+#    #+#             */
-/*   Updated: 2021/06/20 20:18:42 by jfritz           ###   ########.fr       */
+/*   Updated: 2021/06/21 11:21:26 by jfritz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,13 @@ size_t	ft_strlen_char(const char *s, char c)
 	size_t	n;
 	char	*next;
 	size_t	d;
+	size_t	i;
 
 	n = 0;
-	next = ft_strchr((const char *) &s[1], c);
+	i = 0;
+	while (!(s[i] == c && (s[i + 1] != c || s[i + 1] == '\0')))
+		i++;
+	next = (const char*) &s[1 + i];
 	if (!next)
 		return (0);
 	d = (next - s) * sizeof(char);
@@ -41,7 +45,7 @@ char	*ft_get_index(char *s, char c, int p)
 	u = 0;
 	while (s[x])
 	{
-		if (s[x] == c)
+		if ((s[x] == c && (s[x + 1] != c || s[x + 1] == '\0')))
 		{
 			if (p == u)
 			{
@@ -61,14 +65,18 @@ int	ft_word_count(char *s, char c)
 	int	w;
 
 	d = 0;
-	w = 1;
+	w = 0;
 	if (ft_strlen(s) == 0)
 		return (0);
+	if (s[0] != c)
+	{
+		w++;
+	}
 	while (d < (int) ft_strlen(s))
 	{
-		d++;
-		if (s[d] == c)
+		if (s[d] == c && (s[d + 1] != c || s[d + 1] == '\0'))
 			w++;
+		d++;
 	}
 	return (w);
 }
@@ -82,12 +90,23 @@ char	**ft_split(char const *s, char c)
 	e = 0;
 	w = ft_word_count((char *) s, c);
 	d = (char **)malloc(sizeof(char *) * w);
+	printf("Words: %d\n", w);
 	while (e < w)
 	{
 		d[e] = ft_get_index((char *) s, c, e);
+		printf("LOS: %s", d[e]);
 		e++;
 	}
 	if (!d)
 		return (NULL);
 	return (d);
+}
+
+int main()
+{
+	char *to_split = "     tss=1232123=            test1     test2 test3";
+	char placeholder = ' ';
+
+	char **c = ft_split(to_split, placeholder);
+	printf("\nFIRST :%s", c[2]);
 }
