50932140647071754076471fc80d6b46dbfdd2a6
Jonathan Fritz <jfritz@1-B-5.42heilbronn.de>
2021-06-21 14:39:26 +0200

    Unbelievable i made this norminette confirm

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: jfritz <jfritz@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/19 18:38:10 by jfritz            #+#    #+#             */
-/*   Updated: 2021/06/21 14:18:38 by jfritz           ###   ########.fr       */
+/*   Updated: 2021/06/21 14:38:57 by jfritz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ size_t	ft_strlen(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strtrim(char const *s1, char const *set);
 
-size_t	ft_strlen_char(const char *s, char c)
+size_t	ft_clen(const char *s, char c)
 {
 	size_t	n;
 	char	*next;
@@ -29,7 +29,7 @@ size_t	ft_strlen_char(const char *s, char c)
 	{
 		while (s[i] != c)
 			i++;
-		return i;
+		return (i);
 	}
 	while (s[i + 1] != c)
 		i++;
@@ -46,37 +46,27 @@ char	*ft_get_index(const char *s, char c, int p)
 {
 	int		d;
 	int		w;
-	int		l;
-	char	*s1;
-	char	*c1;
 
 	d = 0;
 	w = 0;
-	l = 0;
 	if (ft_strlen(s) == 0)
 		return (0);
 	if (s[0] != c)
 	{
 		w++;
-		l = ft_strlen_char(&s[0], c);
 		if (p == 0)
-			return ft_strtrim(ft_substr(&s[d], 0, l), &c);
+			return (ft_strtrim(ft_substr(&s[d], 0, ft_clen(&s[0], c)), &c));
 	}
-	while (d < (int) ft_strlen(s))
+	while (d++ < (int) ft_strlen(s))
 	{
 		if (s[d] == c && s[d + 1] != c)
 		{
 			if (w == p)
-			{
-				l = ft_strlen_char(&s[d], c);
-				s1 = ft_substr(&s[d], 0, l);
-				return ft_strtrim(ft_substr(&s[d], 0, l), &c);
-			}
+				return (ft_strtrim(ft_substr(&s[d], 0, ft_clen(&s[d], c)), &c));
 			w++;
 		}
-		d++;
 	}
-	return (char *) &s[d];
+	return ((char *) &s[d]);
 }
 
 int	ft_word_count(char *s, char c)
@@ -101,23 +91,21 @@ char	**ft_split(char const *s, char c)
 	char			**d;
 	unsigned int	w;
 	unsigned int	e;
+	char			end;
 
+	end = '\0';
+	if (!s)
+		return (0);
 	e = 0;
 	w = ft_word_count((char *) s, c);
-	d = (char **)malloc(sizeof(char *) * w);
+	d = (char **)malloc((sizeof(char *) * (w + 1)));
+	if (!d)
+		return (NULL);
 	while (e < w)
 	{
 		d[e] = ft_get_index((char *) s, c, e);
 		e++;
 	}
-	if (!d)
-		return (NULL);
+	d[e] = NULL;
 	return (d);
 }
-
-// int	main()
-// {
-// 	char *s = "      split       this for   me  !       ";
-// 	char **c = ft_split(s, ' ');
-// 	printf("%s", c[4]);
-// }
