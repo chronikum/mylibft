b138467c8c4d349bba1e8d4b01ac19ad5b5d90e2
Jonathan Fritz <jfritz@1-B-5.42heilbronn.de>
2021-06-21 14:18:50 +0200

    nice

--- ft_split.c
+++ ft_split.c
@@ -6,15 +6,13 @@
 /*   By: jfritz <jfritz@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/19 18:38:10 by jfritz            #+#    #+#             */
-/*   Updated: 2021/06/21 14:13:00 by jfritz           ###   ########.fr       */
+/*   Updated: 2021/06/21 14:18:38 by jfritz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 size_t	ft_strlen(const char *s);
-char	*ft_strchr(const char *s, int c);
-void	*ft_memchr(const void *s, int c, size_t n);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strtrim(char const *s1, char const *set);
 
@@ -37,7 +35,7 @@ size_t	ft_strlen_char(const char *s, char c)
 		i++;
 	if (!(s[i] == c && (s[i + 1] != c)))
 		i++;
-	next = (const char*) &s[1 + i];
+	next = (char *) &s[1 + i];
 	if (!next)
 		return (0);
 	d = (next - s) * sizeof(char);
@@ -46,10 +44,11 @@ size_t	ft_strlen_char(const char *s, char c)
 
 char	*ft_get_index(const char *s, char c, int p)
 {
-	int	d;
-	int	w;
-	int	l;
-	char *s1;
+	int		d;
+	int		w;
+	int		l;
+	char	*s1;
+	char	*c1;
 
 	d = 0;
 	w = 0;
@@ -60,7 +59,6 @@ char	*ft_get_index(const char *s, char c, int p)
 	{
 		w++;
 		l = ft_strlen_char(&s[0], c);
-		printf("FIRST CHECKING LENGTH: %d\n", l);
 		if (p == 0)
 			return ft_strtrim(ft_substr(&s[d], 0, l), &c);
 	}
@@ -72,15 +70,13 @@ char	*ft_get_index(const char *s, char c, int p)
 			{
 				l = ft_strlen_char(&s[d], c);
 				s1 = ft_substr(&s[d], 0, l);
-				char *c1 = ft_strtrim(ft_substr(&s[d], 0, l), &c);
-				printf("|%s| and length: %d\n", c1, ft_strlen(c1));
-				return c1;
+				return ft_strtrim(ft_substr(&s[d], 0, l), &c);
 			}
 			w++;
 		}
 		d++;
 	}
-	return &s[d];
+	return (char *) &s[d];
 }
 
 int	ft_word_count(char *s, char c)
@@ -109,11 +105,9 @@ char	**ft_split(char const *s, char c)
 	e = 0;
 	w = ft_word_count((char *) s, c);
 	d = (char **)malloc(sizeof(char *) * w);
-	printf("Words: %d\n", w);
 	while (e < w)
 	{
 		d[e] = ft_get_index((char *) s, c, e);
-		printf("%d: |%s|\n",e, d[e]);
 		e++;
 	}
 	if (!d)
@@ -121,11 +115,9 @@ char	**ft_split(char const *s, char c)
 	return (d);
 }
 
-int main()
-{
-	char *to_split = "test0      test1     test2 test3 ";
-	char placeholder = ' ';
-
-	char **c = ft_split(to_split, placeholder);
-	printf("\nFIRST :%s", c[3]);
-}
+// int	main()
+// {
+// 	char *s = "      split       this for   me  !       ";
+// 	char **c = ft_split(s, ' ');
+// 	printf("%s", c[4]);
+// }
