13db9baaff8a8edb70fc692b02dee14452648c5c
Jonathan Fritz <jfritz@1-B-5.42heilbronn.de>
2021-06-22 11:44:53 +0200

    norm lstnew

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,18 +6,19 @@
 /*   By: jfritz <jfritz@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/21 15:53:24 by jfritz            #+#    #+#             */
-/*   Updated: 2021/06/21 15:58:33 by jfritz           ###   ########.fr       */
+/*   Updated: 2021/06/22 11:44:34 by jfritz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list *ft_lstnew(void *content)
+t_list	*ft_lstnew(void *content)
 {
-	t_list *s;
+	t_list	*s;
+
 	s = malloc(sizeof(t_list));
 	if (!s)
 		return (0);
 	s->content = content;
-	return s;
+	return (s);
 }
