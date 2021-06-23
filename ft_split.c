b520cf9c443ddaff37af5293f2fda464a48b4d63
Jonathan Fritz <jfritz@1-B-5.42heilbronn.de>
2021-06-23 08:47:05 +0200

    Fixing ft split

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: jfritz <jfritz@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/22 17:15:44 by jfritz            #+#    #+#             */
-/*   Updated: 2021/06/22 22:03:05 by jfritz           ###   ########.fr       */
+/*   Updated: 2021/06/23 08:46:54 by jfritz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,44 +15,54 @@
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 size_t	ft_strlen(const char *s);
 
-int  ft_protected(char ***ret, char *s, int (*arr)[3])
+int  ft_create_result(char ***result, char *s)
 {
 	if (!s)
 		return (0);
-	*ret = malloc(sizeof(char *) * ft_strlen(s) + 1);
-	if (!*ret)
+	*result = malloc(sizeof(char *) * ft_strlen(s) + 1);
+	if (!*result)
 		return (0);
-	(*arr)[0] = 0;
-	(*arr)[1] = 0;
 	return (1);
 }
 
 char **ft_split(char const *s, char c)
 {
-	char	**ret;
+	char	**result;
 	int		i;
-	int		arr[3];
+	int		j;
+	int		k;
+	int		l;
 
 	i = 0;
-	if (!ft_protected(&ret, (char *) s, &arr))
+	j = 0;
+	l = 0;
+	if (!ft_create_result(&result, (char *) s))
 		return (0);
-	while (s[arr[0]]) // Für jeden Char
+	while (s[i])
 	{
-		while (s[arr[0]] == c && s[arr[0]])
-			arr[0]++;
-		// if (s[arr[0]])
-		// {
-		// 	arr[2] = arr[0];
-		// 	while (s[arr[0]] != c && s[arr[0]])
-		// 		arr[0]++;
-		// 	ret[arr[1]] = malloc(arr[0] - arr[2] + 1);
-		// 	ft_memcpy(ret[arr[1]], &(s[arr[2]]), arr[0] - arr[2]);
-		// 	ret[arr[1]][arr[0] - arr[2]] = 0;
-		// 	arr[1]++;
-		// }
-		if (s[arr[0]])
-			arr[0]++;
+		while (s[i] == c && s[i])
+			i++;
+		if (s[i])
+		{
+			j = i;
+			while (s[i] != c && s[i])
+				i++;
+			result[l] = malloc(i - j + 1);
+			ft_memcpy(result[l], &(s[j]), i - j);
+			result[l][i - j] = 0;
+			l++;
+		}
+		if (s[i])
+			i++;
 	}
-	ret[arr[1]] = 0;
-	return (ret);
+	result[l] = 0;
+	return (result);
+}
+
+int	main()
+{
+	char *s = "Das ist ein Test!";
+	char p = ' ';
+
+	char **r = ft_split(s, p);
 }
